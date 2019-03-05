d90ea335c11bd6d351ec08bdde4f71e880f68cda
Tommaso Brega <tommy.brega@gmail.com>
2019-03-06 00:52:52 +0100

    Update Weapons.h

--- Weapons.h
+++ Weapons.h
@@ -1,14 +1,17 @@
 #pragma once
-#include "Utility.h"
-#include <TL-Engine.h>	// TL-Engine include file and namespace
+
+#include "TL-Engine.h"
+#include <string>
 #include <deque>
+#include <TL-Engine.h>
 using namespace tle;
 
 class CRifle
 {
 private:
-	int mSpeed;
+	I3DEngine* mEngine;
 	IMesh* bulletMesh;
+	int mSpeed;
 public:
 	CRifle(I3DEngine* myEngine, string meshName, int speed);
 	~CRifle(){};
@@ -21,57 +24,6 @@ public:
 	void move(int speed);
 	void Delete();
 	void BulletCheck(float playerxPos, float playerzPos, float playerRad, deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr);
+	bool getDistance(float s1xPos, float s1zPos, float s1rad, float s2xPos, float s2zPos, float s2rad);
+
 };
-
-//initialising the game with the existing tlEngine and the wanted speed of the bullet
-CRifle::CRifle(I3DEngine* myEngine, string meshName, int speed)
-{
-	bulletMesh = myEngine->LoadMesh(meshName);
-	mSpeed = speed;
-}
-
-//Creating an instance of the bullet
-bool CRifle::buildBullet(int x, int y, int z)
-{
-	bulletModel = bulletMesh->CreateModel(x, y, z);
-	return bulletModel;
-}
-
-//Gets the spwaing position of the bullet
-float CRifle::GetPosition(float& x, float& z)
-{
-	x = bulletModel->GetX();
-	z = bulletModel->GetZ();
-	return x, z;
-}
-
-//Moving the bullet foward
-void CRifle::move(int speed)
-{
-	bulletModel->MoveLocalZ(speed);
-}
-
-//Delte the bulletModel
-void CRifle::Delete()
-{
-	bulletMesh->RemoveModel(bulletModel);
-}
-
-
-void CRifle::BulletCheck(float playerxPos, float playerzPos, float playerRad, deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr)
-{
-	auto p = bulletList.begin();
-	while (p != bulletList.end())
-	{
-		(*p)->GetPosition(bulletxPos, bulletzPos);
-		bool BulletOutRange = utility::getDistance(playerxPos, playerzPos, playerRad, bulletxPos, bulletzPos, 2.0f);
-
-		if (BulletOutRange == false)
-		{
-			(*p)->Delete();
-			bulletList.erase(p);
-			break;
-		}
-		p++;
-	}
-}
