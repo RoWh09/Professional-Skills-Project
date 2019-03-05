f30cee84fb38c05c3e7c8a0188fbe588dbf821f1
Tommaso Brega <tommy.brega@gmail.com>
2019-03-06 00:52:01 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <string>
 #include "Weapons.h"
+#include <string>
 #include "TL-Engine.h"
 #include <TL-Engine.h>
 using namespace tle;
@@ -10,55 +10,15 @@ class CPlayer
 {
 private:
 	I3DEngine* mEngine;
-	IModel* playerModel;
-	IModel* mDummy;
 	string mName;
 	int mHealth;
 	int mAmo;
 
 public:
-	CPlayer(IModel* dummy, IModel* player, I3DEngine* myEngine, string name, int health, int amo);
+	IModel* playerModel;
+	CPlayer(IMesh* playerMesh ,I3DEngine* myEngine, string name, int health, int amo);
 	void Move();
-	void Shoot(deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr);
+	bool Shoot(IModel* dummyModel, deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr, int x, int y, int z);
+	bool ClearBullet(IModel* dummyModel, deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr);
 };
 
-CPlayer::CPlayer (IModel* dummy, IModel* player, I3DEngine* myEngine, string name, int health, int amo)
-{
-	playerModel = player;
-	mEngine = myEngine;
-	mName = name;
-	mHealth = health;
-	mAmo = amo;
-}
-
-void CPlayer::Move()
-{
-	if (mEngine->KeyHeld(Key_W))
-	{
-		playerModel->MoveZ(1);
-		mDummy->MoveZ(1);
-	}
-	if (mEngine->KeyHeld(Key_S))
-	{
-		playerModel->MoveZ(-1);
-		mDummy->MoveZ(-1);
-	}
-	if (mEngine->KeyHeld(Key_D))
-	{
-		playerModel->MoveX(1);
-		mDummy->MoveX(1);
-	}
-	if (mEngine->KeyHeld(Key_A))
-	{
-		playerModel->MoveX(-1);
-		mDummy->MoveX(-1);
-	}
-}
-
-void CPlayer::Shoot(deque <unique_ptr < CRifle > >& bulletList, unique_ptr<CRifle>&bulletPtr)
-{
-		bulletPtr.reset(new CRifle(mEngine, "cube.x", 1.0));
-		bulletPtr->buildBullet(playerModel->GetX(), playerModel->GetY(), playerModel->GetZ());
-		bulletPtr->bulletModel->LookAt(mDummy);
-		bulletList.push_back(move(bulletPtr));
-}
